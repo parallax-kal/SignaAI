12409a6b5765c41f176176050fdcd8907b0962b5
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2024-02-11 06:28:03 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,105 @@
+import speech_recognition as sr
+import os
+import pygame
+import cv2
+import time
+import re
+
+
+# Constants
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 400
+
+pygame.init()
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Speech to Sign Language')
+
+
+class SpeechToSignLanguage:
+    def __init__(self, screen_rect):
+        self.screen_rect = screen_rect
+        self.r = sr.Recognizer()
+        self.specifics = self.get_videos("assets/specific/")
+        self.alpha_numerics = self.get_videos("assets/alpha_numeric/")
+        
+    def decontracted(self, phrase):
+        # specific
+        phrase = re.sub(r"won\'t", "will not", phrase)
+        phrase = re.sub(r"can\'t", "can not", phrase)
+
+        # general
+        phrase = re.sub(r"n\'t", " not", phrase)
+        phrase = re.sub(r"\'re", " are", phrase)
+        phrase = re.sub(r"\'s", " is", phrase)
+        phrase = re.sub(r"\'d", " would", phrase)
+        phrase = re.sub(r"\'ll", " will", phrase)
+        phrase = re.sub(r"\'t", " not", phrase)
+        phrase = re.sub(r"\'ve", " have", phrase)
+        phrase = re.sub(r"\'m", " am", phrase)
+        return phrase
+
+
+    def get_videos(self, folder_path) -> list[str]:
+        file_names = os.listdir(folder_path)
+        file_names = [file_name.split(".")[0] for file_name in file_names]
+        return file_names
+
+    def record_transcribe(self) -> str:
+        while (1):
+            try:
+                with sr.Microphone() as source2:
+                    self.r.adjust_for_ambient_noise(source2, duration=0.2)
+                    audio2 = self.r.listen(source2)
+                    MyText = self.r.recognize_google(audio2)
+                    print(MyText)
+                    return MyText
+            except:
+                print("Error occurred")
+
+    def play_video(self, video_file_path: str, word: str):
+        cap = cv2.VideoCapture(video_file_path)
+        while (cap.isOpened()):
+            ret, frame = cap.read()
+            if ret:
+                frame = cv2.resize(frame, (WINDOW_WIDTH, WINDOW_HEIGHT))
+                frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                
+                cv2.putText(frame, word, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA, True)
+                
+                frame = pygame.surfarray.make_surface(frame)
+                screen.blit(frame, (0, 0))
+                pygame.display.flip()
+                time.sleep(1/30)
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        cap.release()
+                        pygame.quit()
+                        exit()
+            else:
+                break
+        cap.release()
+
+    def start(self):
+        while (1):
+            text = self.record_transcribe()
+            text = self.decontracted(text)
+            splitted_text = text.split(" ")
+            specific_path = "assets/specific/"
+            alpha_numeric_path = "assets/alpha_numeric/"
+            for word in splitted_text:
+                if word in self.specifics:
+                    print(word)
+                    self.play_video(specific_path+word+".mp4", word)
+                else:
+                    for char in word:
+                        if char in self.alpha_numerics:
+                            print(char)
+                            self.play_video(alpha_numeric_path+char+".mp4", word)
+                        else:
+                            print("Not found")
+                            continue
+
+
+speech_to_sign_language = SpeechToSignLanguage(screen.get_rect())
+speech_to_sign_language.start()
